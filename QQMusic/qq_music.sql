3730fdc6c7f9f0db1dbdb8750182d7421af33611
JianpengHe <i@hejianpeng.cn>
2023-01-12 00:25:32 +0800

    feat: 创建下载列表

--- QQMusic/qq_music.sql
+++ QQMusic/qq_music.sql
@@ -1,17 +1,33 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 CREATE DATABASE IF NOT EXISTS `qq_music` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `qq_music`;
 
 DELIMITER $$
+DROP PROCEDURE IF EXISTS `创建下载列表`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `创建下载列表` ()  NO SQL
+BEGIN
+
+UPDATE `media` SET `is_vip`=null,`down_mid`=null;
+
+UPDATE (SELECT media_mid,min(is_vip) as need_vip FROM song GROUP BY media_mid)b INNER join media on media.media_mid=b.media_mid set media.is_vip=b.need_vip WHERE media.media_mid in (SELECT media_mid FROM song WHERE album_id in (SELECT albumID FROM album)) and (	size_96aac>0 or size_320mp3>0 or size_flac>0);
+
+UPDATE (SELECT song.media_mid,mid FROM media INNER JOIN song on media.media_mid=song.media_mid WHERE media.is_vip=song.is_vip) a INNER JOIN media on a.media_mid=media.media_mid set media.down_mid=a.mid;
+
+/*UPDATE (SELECT media_mid,is_vip,mid FROM song WHERE album_id in (SELECT albumID FROM album) ORDER BY is_vip ASC)b INNER join media on media.media_mid=b.media_mid set media.is_vip=b.is_vip,media.down_mid=b.mid;*/
+
+SELECT * FROM (SELECT * FROM (SELECT media_mid,avg(is_vip) as n FROM `song` WHERE 1 GROUP BY `media_mid` ) a WHERE n<1 and n>0) b INNER JOIN media on b.media_mid=media.media_mid ORDER BY `media`.`is_vip` DESC;
+END$$
+
 DROP PROCEDURE IF EXISTS `统计`$$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `统计` ()  NO SQL
 SELECT * FROM (SELECT grp_from_song_id as id,COUNT(*) as num ,sum(comment_count) as total_comment_count FROM `song` GROUP by grp_from_song_id) a INNER JOIN song b on a.id=b.song_id  
 ORDER BY `a`.`total_comment_count` DESC$$
 
 DROP PROCEDURE IF EXISTS `统计应下载的大小`$$
-CREATE DEFINER=`root`@`127.0.0.1` PROCEDURE `统计应下载的大小` ()  SELECT sum(size_96aac)/1024/1024/1024 as `acc大小(GB)`,sum(size_320mp3)/1024/1024/1024 as `mp3大小(GB)`,sum(size_flac)/1024/1024/1024 as `flac大小(GB)` FROM `media`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `统计应下载的大小` ()  SELECT sum(size_96aac)/1024/1024/1024 as `aac大小(GB)`,sum(size_320mp3)/1024/1024/1024 as `mp3大小(GB)`,sum(size_flac)/1024/1024/1024 as `flac大小(GB)` FROM `media` WHERE is_vip is not null$$
 
 DELIMITER ;
 
@@ -33,7 +49,12 @@ CREATE TABLE `media` (
   `media_mid` varchar(32) NOT NULL,
   `size_96aac` int(1) UNSIGNED DEFAULT NULL,
   `size_320mp3` int(1) UNSIGNED DEFAULT NULL,
-  `size_flac` int(1) UNSIGNED DEFAULT NULL
+  `size_flac` int(1) UNSIGNED DEFAULT NULL,
+  `is_vip` tinyint(1) UNSIGNED DEFAULT NULL,
+  `down_mid` varchar(32) DEFAULT NULL,
+  `disk_size_96aac` int(1) DEFAULT NULL,
+  `disk_size_320mp3` int(1) DEFAULT NULL,
+  `disk_size_flac` int(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 DROP TABLE IF EXISTS `singer`;
