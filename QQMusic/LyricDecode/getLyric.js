e4c1c36f330c2df3bd771dfe36856dff44ae6c5a
何健鹏 <i@hejianpeng.cn>
2023-01-18 13:23:44 +0800

    feat: 更新歌词LyricDecode C++ Addon，不再需要dll

--- QQMusic/LyricDecode/getLyric.js
+++ QQMusic/LyricDecode/getLyric.js
@@ -1,20 +1,14 @@
-const { LyricDecode } = require(`./QQMusicLyricDecode_node-v${
-  process.version.match(/^v(\d{1,2})\./)[1]
-}-win-x86.node`);
+const { LyricDecode } = require(`./index`);
 const { encode } = require("./convert");
 const { request, GetPlayLyricInfo } = require("../comm/index");
 const zlib = require("zlib");
 const fs = require("fs");
 const PATH = __dirname + "/lyric_file/";
 const save_request_cache = ({ data: { songID, lyric }, item }) => {
-  fs.writeFile(
-    PATH + (songID || item?.param?.songID) + ".qqlyric",
-    Buffer.from(lyric, "hex"),
-    () => {}
-  );
+  fs.writeFile(PATH + (songID || item?.param?.songID) + ".qqlyric", Buffer.from(lyric, "hex"), () => {});
 };
-const read_request_cache = (ob) =>
-  new Promise((r) => {
+const read_request_cache = ob =>
+  new Promise(r => {
     const { songID } = ob.item.param;
     fs.readFile(PATH + songID + ".qqlyric", (err, lyric) => {
       if (lyric) {
@@ -23,22 +17,19 @@ const read_request_cache = (ob) =>
       r();
     });
   });
-const getLyric = async (song_ids) =>
+const getLyric = async song_ids =>
   await Promise.all(
     (
       await request(
-        (song_ids[0].song_id
-          ? song_ids.map(({ song_id }) => song_id)
-          : song_ids
-        ).map(GetPlayLyricInfo),
-        (ob) => {
+        (song_ids[0].song_id ? song_ids.map(({ song_id }) => song_id) : song_ids).map(GetPlayLyricInfo),
+        ob => {
           ob.save_request_cache = save_request_cache;
           ob.read_request_cache = read_request_cache;
         }
       )
     ).map(
       ({ lyric, songID }, i) =>
-        new Promise((r) => {
+        new Promise(r => {
           if (!(lyric instanceof Buffer)) {
             lyric = Buffer.from(lyric || "", "hex");
           }
@@ -49,9 +40,7 @@ const getLyric = async (song_ids) =>
                 r({ lyric: null, song_id: songID });
                 return;
               }
-              const delSongInfos = song_ids[i].singer
-                ? String(song_ids[i].singer).split("、")
-                : [];
+              const delSongInfos = song_ids[i].singer ? String(song_ids[i].singer).split("、") : [];
               // song_ids[i].name && delSongInfos.push(song_ids[i].name);
 
               r({ lyric: encode(String(d), delSongInfos), song_id: songID });
